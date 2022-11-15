41461aa6834e66afc2978cdca81da39d70f6a6b8
gusarovf <6527375@mail.ru>
2022-11-15 14:09:57 +0300

    Added credit org type description.

--- src/source/types.ts
+++ src/source/types.ts
@@ -1,19 +1,21 @@
 export type CbrCoWorkSheet = [{ name: string; data: string[][] }]
 
 export interface CreditOrganisation {
-  ccode: number
-  oldctbank: string
-  newctbank: string | null
-  csname: string
-  cnamer: string
-  oldcopf: string
-  newcopf: string
-  cregnum: number
-  oldcregnr: string
-  newcregnr: string | null
-  cdreg: string
-  lic: string | null
-  strcuraddr: string
-  ogrn: string
-}
+  ccode: number // id центробанка
+  oldctbank: string // Вид
+  newctbank: string | null // Вид
+  csname: string // Сокращённое фирменное наименование
+  cnamer: string // Полное фирменное наименование
+  oldcopf: string // Организационно-правовая форма
+  newcopf: string // Организационно-правовая форма (новое значение)
+  
+  // Регистрационный номер
+  cregnum: number 
+  oldcregnr: string 
+  newcregnr: string | null 
 
+  cdreg: string // Дата регистрации ЦБ
+  lic: string | null // Статус лицензии
+  strcuraddr: string // Местонахождение
+  ogrn: string // ОГРН
+}
